235e2ea21b3bcc86337817789b35c65c1fd52604
Nicolas <nicolasmoneteiro8@gmail.com>
2025-05-20 09:35:27 -0300

    cria endpoints PUT e DELETE

--- src/main/java/com/mycompany/api/de/cadastro/ProductResource.java
+++ src/main/java/com/mycompany/api/de/cadastro/ProductResource.java
@@ -5,15 +5,23 @@
 package com.mycompany.api.de.cadastro;
 
 import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.Response;
 import java.util.List;
 
 @Path("/products")
 public class ProductResource {
     
+    @PersistenceContext
+    private EntityManager em;
     @Inject
     private ProductService productService;
     
@@ -28,6 +36,33 @@ public class ProductResource {
         return Response.status(Response.Status.CREATED).entity(product).build();
     }
     
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public Response atualizar(@PathParam("id") Long id, Product product) {
+        
+           Product produtoExistente = em.find(Product.class, id) ;
+        if (produtoExistente == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        } 
+           produtoExistente.setNome(product.getNome());
+           produtoExistente.setPreco(product.getPreco());
+           produtoExistente.setQuantidade(product.getQuantidade());
+           
+           em.merge(produtoExistente);
+           return Response.ok().build();       
+    }
     
-   
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public Response deletar(@PathParam("id") Long id) {
+          Product produtoExistente = em.find(Product.class, id) ;
+        if (produtoExistente == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }  
+            em.remove(produtoExistente);
+            return Response.ok().build();
+    }
+    
 }
